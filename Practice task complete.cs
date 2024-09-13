c0433e8abfaa4ecfaaad5a0edb2cda65faea734f
HasnainH311 <19hhaider@heckgrammar.co.uk>
2024-09-13 09:39:41 +0100

    Add files via upload

---
+++ Practice task complete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCSE_Practice_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Console.WriteLine("Enter your full name");
+            userName = Console.ReadLine();
+            Console.WriteLine("Your name is " + userName);
+            Console.Write("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
